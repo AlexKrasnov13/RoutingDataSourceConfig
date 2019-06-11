56f8bdf8b3e0adfc00e724ea361e7d15ec17ab07
Alex Krasnov <h-kot@rambler.ru>
2019-06-11 08:38:49 +0300

    Update RoutingDataSource.java

--- datasource/RoutingDataSource.java
+++ datasource/RoutingDataSource.java
@@ -1,12 +1,8 @@
-package ru.mdimension.mda.lib.db.datasource;
+package ru.lib.db.datasource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-
-/**
- * @author akrasnov on 16.05.2019
- */
 
 public class RoutingDataSource extends AbstractRoutingDataSource {
     private final Logger log = LoggerFactory.getLogger(RoutingDataSource.class);
